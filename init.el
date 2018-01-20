8ff1d6978047766061b11ef10cc4ce51baad69b8
Shea Parkes <shea.parkes@milliman.com>
2018-01-19 22:45:25 -0500

    Switch to ivy from helm.
    Do this just for performance reasons while I'm working on a mobile proc.

--- init.el
+++ init.el
@@ -57,6 +57,7 @@ values."
      yaml
      salt
      ess
+     ivy
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
